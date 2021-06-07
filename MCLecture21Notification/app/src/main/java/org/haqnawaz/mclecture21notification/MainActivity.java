0cf3fd6f59ef9d1424a6167342cd81472c995463
Ayman <bsef18m021@pucit.edu.pk>
2021-06-08 02:38:42 +0500

    First commit

---
+++ MCLecture21Notification/app/src/main/java/org/haqnawaz/mclecture21notification/MainActivity.java
@@ -0,0 +1,14 @@
+package org.haqnawaz.mclecture21notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
