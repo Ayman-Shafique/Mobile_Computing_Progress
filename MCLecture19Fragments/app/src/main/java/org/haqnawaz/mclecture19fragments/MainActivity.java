4aa135e0bc04e7d34aa19364211d356cb28d5538
Ayman <bsef18m021@pucit.edu.pk>
2021-06-08 00:50:16 +0500

    Fragment 1 added

--- MCLecture19Fragments/app/src/main/java/org/haqnawaz/mclecture19fragments/MainActivity.java
+++ MCLecture19Fragments/app/src/main/java/org/haqnawaz/mclecture19fragments/MainActivity.java
@@ -1,14 +1,34 @@
 package org.haqnawaz.mclecture19fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Button1;
+    Button Button2;
+    Button Button3;
+    LinearLayout myLinearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button1 = findViewById(R.id.btn_1);
+        myLinearLayout = findViewById(R.id.linearLayout);
+
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment1 frag1 = new Fragment1();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout, frag1);
+                transaction.commit();
+            }
+        });
     }
 }
